a0d8ef109b6b1ca6c3a9597901ce7ea34afc856a
satyamsingh07 <satyamsingh20027@gmail.com>
2022-03-03 23:55:26 +0530

    Modal added, everything done except changes in timeline, faq content and about us content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,11 +15,20 @@ export default function Footer(props) {
       <div className="footer-container">
         <div className="footer-left">
           <span style={{ display: "flex", justifyContent: "center" }}>
-            <h1 style={{ color: "lime", padding: "5px",fontWeight:'lighter',paddingBottom:20 }}>
-              ANDROID CLUB VIT®
+            <h1
+              style={{
+                color: "lime",
+                padding: "5px",
+                fontWeight: "lighter",
+                paddingBottom: 20,
+              }}
+            >
+              Android Club Vit®
             </h1>
           </span>
-          <h4 style={{ padding: "10px", color: "white",fontWeight:'lighter' }}>
+          <h4
+            style={{ padding: "10px", color: "white", fontWeight: "lighter" }}
+          >
             Android club is for developers who are interested in androids
             developer technology; everything from the Android, Chrome, Drive,
             and Google Cloud platforms, to product APIs like the Cast API, Maps
@@ -36,12 +45,14 @@ export default function Footer(props) {
               alignItems: "center",
               justifyContent: "flex-start",
               paddingLeft: 30,
-              paddingTop:50,
+              paddingTop: 50,
               backgroundColor: "black",
             }}
           >
             <FontAwesomeIcon icon={faLocationDot} color={"lime"} size="2xl" />
-            <h4 style={{ marginLeft: 15,color:'white',fontWeight:'lighter' }}>
+            <h4
+              style={{ marginLeft: 15, color: "white", fontWeight: "lighter" }}
+            >
               Find Us: Vellore Institute of Technology, Chennai{" "}
             </h4>
           </div>
@@ -57,7 +68,9 @@ export default function Footer(props) {
           >
             {" "}
             <FontAwesomeIcon icon={faEnvelope} color={"lime"} size="2xl" />
-            <h4 style={{ marginLeft: 15,color:'white',fontWeight:'lighter'  }}>
+            <h4
+              style={{ marginLeft: 15, color: "white", fontWeight: "lighter" }}
+            >
               Mail Us at: androidclub@vit.ac.in{" "}
             </h4>
           </div>
@@ -73,26 +86,38 @@ export default function Footer(props) {
               backgroundColor: "black",
             }}
           >
-            <FontAwesomeIcon icon={faInstagram} size="2xl" color={"lime"} />
-            <FontAwesomeIcon icon={faLinkedin} size="2xl"  color={"lime"} />
+            <FontAwesomeIcon icon={faInstagram} size="2xl" color={"lime"} onClick={(e) => {
+      e.preventDefault();
+      window.location.href='https://www.instagram.com/androidvitc'}}/>
+            <FontAwesomeIcon icon={faLinkedin} size="2xl" color={"lime"} onClick={(e) => {
+      e.preventDefault();
+      window.location.href='https://www.linkedin.com/company/android-club-vitc/mycompany/'}}/>
           </div>
         </div>
 
         <div className="footer-right">
-        <span style={{ display: "flex", justifyContent: "center" }}>
-            <h1 style={{ color: "lime", padding: "5px",fontWeight:'lighter',paddingBottom:20 }}>
+          <span style={{ display: "flex", justifyContent: "center" }}>
+            <h1
+              style={{
+                color: "lime",
+                padding: "5px",
+                fontWeight: "lighter",
+                paddingBottom: 20,
+              }}
+            >
               Linux Club Vit®
             </h1>
           </span>
-          <h4 style={{ padding: "10px", color: "white",fontWeight:'lighter' }}>
-            Android club is for developers who are interested in androids
-            developer technology; everything from the Android, Chrome, Drive,
-            and Google Cloud platforms, to product APIs like the Cast API, Maps
-            API, and YouTube API are being used by the students to build apps. A
-            club can take many forms from just a few people getting together, to
-            large gatherings with demos and tech talks, to events like code
-            sprints and hack-a-thon’s. However, at core, android club focuses on
-            development of technical content
+          <h4
+            style={{ padding: "10px", color: "white", fontWeight: "lighter" }}
+          >
+            The 'Linux Club' of VIT, Chennai was created to Spread the awareness
+            of Free and Open Source Software (FOSS) through Linux flavours. It
+            believes in developing collaborative open-source projects. It
+            encourages users to understand the systems and softwares they use on
+            a fundamental level. Which can be achieved only using
+            non-proprietary tools. It aims to get member to contribute to FOSS,
+            make them more industry ready and to promote GNU/Linux in General.
           </h4>
 
           <div
@@ -101,12 +126,14 @@ export default function Footer(props) {
               alignItems: "center",
               justifyContent: "flex-start",
               paddingLeft: 30,
-              paddingTop:50,
+              paddingTop: 50,
               backgroundColor: "black",
             }}
           >
-            <FontAwesomeIcon icon={faLocationDot}  color={"lime"}  size="2xl" />
-            <h4 style={{ marginLeft: 15,color:'white',fontWeight:'lighter'  }}>
+            <FontAwesomeIcon icon={faLocationDot} color={"lime"} size="2xl" />
+            <h4
+              style={{ marginLeft: 15, color: "white", fontWeight: "lighter" }}
+            >
               Find Us: Vellore Institute of Technology, Chennai{" "}
             </h4>
           </div>
@@ -121,9 +148,11 @@ export default function Footer(props) {
             }}
           >
             {" "}
-            <FontAwesomeIcon icon={faEnvelope}  color={"lime"}  size="2xl" />
-            <h4 style={{ marginLeft: 15,color:'white',fontWeight:'lighter'  }}>
-              Mail Us at: androidclub@vit.ac.in{" "}
+            <FontAwesomeIcon icon={faEnvelope} color={"lime"} size="2xl"   />
+            <h4
+              style={{ marginLeft: 15, color: "white", fontWeight: "lighter" }}
+            >
+              Mail Us at: linuxclub@vit.ac.in{" "}
             </h4>
           </div>
           <div
@@ -138,12 +167,27 @@ export default function Footer(props) {
               backgroundColor: "black",
             }}
           >
-            <FontAwesomeIcon icon={faInstagram}  color={"lime"}  size="2xl" />
-            <FontAwesomeIcon icon={faLinkedin}   color={"lime"}  size="2xl" />
-         
+            <FontAwesomeIcon icon={faInstagram} color={"lime"} size="2xl" onClick={(e) => {
+      e.preventDefault();
+      window.location.href='https://www.instagram.com/lugvitc/';
+      }}/>
+            <FontAwesomeIcon icon={faLinkedin} color={"lime"} size="2xl" onClick={(e) => {
+      e.preventDefault();
+      window.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+      }}  />
+          </div>
         </div>
+        <div
+          style={{
+            width: "100%",
+            padding: 50,
+            display: "flex",
+            justifyContent: "center",
+            color: "white",
+          }}
+        >
+          <h1>© 2022</h1>{" "}
         </div>
-        <div style={{width:'100%',padding:50,display:'flex',justifyContent:'center',color:'white'}}><h1>© 2022</h1> </div>
       </div>
     </>
   );
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,8 +2,32 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { Fade } from "react-reveal";
 import './index.css';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 export default function Landing(props) {
+    const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    flexDirection:'column',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 'auto',
+    height:'auto',
+    bgcolor: "#000D1a",
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
     return (<>
             <canvas id="matrix">
             </canvas>
@@ -37,11 +61,14 @@ export default function Landing(props) {
                     $(Event.Date): 17th March 2021
                 </div>
                 <div className="landing-buttons">
-                    <button className="landing-register">
+                    <button className="landing-register" onClick={handleOpen}>
                         REGISTER
                         <i className="fa-solid fa-right-to-bracket"></i>
                     </button>
-                    <button className="landing-register landing-discord">
+                    <button className="landing-register landing-discord" onClick={(e) => {
+      e.preventDefault();
+      window.location.href='https://discord.gg/xK23wBty';
+      }}>
                         <i className="fab fa-discord"></i>
                         DISCORD
                     </button>
@@ -51,6 +78,23 @@ export default function Landing(props) {
                 <script scr={require("./script.js")} defer />
             </Helmet>
         </div>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2" color="common.white">
+          <h1 sx={{color:'white'}}> Registrations</h1>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }} color="common.white">
+          <h1 sx={{fontColor:'white'}}> Opening Soon!</h1>
+          
+          </Typography>
+          <Button  variant="contained" onClick={handleClose}>Close</Button>
+        </Box>
+      </Modal>
     </>
     );
 }
